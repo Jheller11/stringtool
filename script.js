e191f48238521a03e1c95c73238c358529e9ddcc
Jeffrey Heller <heller.jeffrey@gmail.com>
2022-08-06 20:25:33 -0400

    Further scripting, HTML framework, import Bulma

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-console.log('script running')
+console.log('Ready to go!')
+// Declare variables for processing input in determineUniqueFeatures()
+let combinedFeatures = []
+let uniqueToOne = []
+let uniqueToTwo = []
+
+// Identify inputs, buttons, results containers
+let inputOne = document.querySelector('#editor-one')
+let inputTwo = document.querySelector('#editor-two')
+let uniqueListOne = document.querySelector('#unique-list-one')
+let sharedList = document.querySelector('#shared-list')
+let uniqueListTwo = document.querySelector('#unique-list-two')
+let compareButton = document.querySelector('#compare-button')
 
 // test strings
 let stringOne =
-  'word,both otherword,,  test testtwo testthree, word,wordtwo,otherword'
-let stringTwo = 'otherword, both both, testfive, testsix,testseven eight'
+  'oneOnly,oneOnly,oneOnly2,  bothone,, bothtwo, boththree  bothfour,bothfour'
+let stringTwo =
+  'twoOnly,twoOnly,twoOnly2,  bothone,, bothtwo, boththree  bothfour,bothfour'
+// test strings END
 
+// Generate a new array of unique values from the input string
 const splitString = (string) => {
   let featuresArray = string.split(/,| /)
-  return featuresArray.filter((s) => s.length > 0)
+  return [...new Set(featuresArray.filter((s) => s.length > 0))]
 }
 
-const compareFeatures = (one, two) => {
-  let allFeatures = one.concat(two).sort()
-  let uniqueFeatures = [...new Set(allFeatures)]
-  return uniqueFeatures
+// Compare the two arrays and place into buckets: Unique vs. Shared
+const determineUniqueFeatures = (A, B) => {
+  A.forEach((word) => {
+    B.includes(word) ? combinedFeatures.push(word) : uniqueToOne.push(word)
+  })
+  B.forEach((word) => {
+    A.includes(word) ? null : uniqueToTwo.push(word)
+  })
 }
 
-console.log(compareFeatures(splitString(stringOne), splitString(stringTwo)))
+determineUniqueFeatures(splitString(stringOne), splitString(stringTwo))
+
+// const createSharedList = (one, two) => {
+//   let allFeatures = one.concat(two).sort()
+//   let combinedFeatures = [...new Set(allFeatures)]
+//   return combinedFeatures
+// }
