0226d2ae8ed156a8f3fa3e9b28dc97ec4da0d459
Jeffrey Heller <heller.jeffrey@gmail.com>
2022-08-07 14:08:10 -0400

    Footer, Header, About section and toggle

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-console.log('Ready to go!')
+// Declare variables for processing input in processFeaturesToLists()
+let combinedFeaturesArray = []
+let uniqueFeaturesArrayOne = []
+let uniqueFeaturesArraytwo = []
 
-// Declare variables for processing input in determineUniqueFeatures()
-let combinedFeatures = []
-let uniqueToOne = []
-let uniqueToTwo = []
-
-// Identify inputs, buttons, results containers
-let inputOne = document.querySelector('#features-one')
-let inputTwo = document.querySelector('#features-two')
+// Textarea Inputs
+let featuresInputOne = document.querySelector('#features-one')
+let featuresInputTwo = document.querySelector('#features-two')
+// List containers for results
 let uniqueListOne = document.querySelector('#unique-list-one')
 let sharedList = document.querySelector('#shared-list')
 let uniqueListTwo = document.querySelector('#unique-list-two')
+// Control Buttons
 let compareButton = document.querySelector('#compare-button')
 let clearButton = document.querySelector('#clear-button')
+// Hero "About"
+let hero = document.querySelector('.hero')
+let aboutButton = document.querySelector('#about')
 
 // Generate a new array of unique values from the input string
 const splitString = (string) => {
@@ -21,22 +24,24 @@ const splitString = (string) => {
 }
 
 // Compare the two arrays and place into buckets: Unique vs. Shared
-const determineUniqueFeatures = (A, B) => {
+const processFeaturesToLists = (A, B) => {
   A.forEach((word) => {
-    B.includes(word) ? combinedFeatures.push(word) : uniqueToOne.push(word)
+    B.includes(word)
+      ? combinedFeaturesArray.push(word)
+      : uniqueFeaturesArrayOne.push(word)
   })
   B.forEach((word) => {
-    A.includes(word) ? null : uniqueToTwo.push(word)
+    A.includes(word) ? null : uniqueFeaturesArraytwo.push(word)
   })
 }
 
 // Clear inputs, lists, and arrays
 const resetPage = () => {
-  inputOne.value = ''
-  inputTwo.value = ''
-  combinedFeatures = []
-  uniqueToOne = []
-  uniqueToTwo = []
+  featuresInputOne.value = ''
+  featuresInputTwo.value = ''
+  combinedFeaturesArray = []
+  uniqueFeaturesArrayOne = []
+  uniqueFeaturesArraytwo = []
   uniqueListOne.innerHTML = ''
   uniqueListTwo.innerHTML = ''
   sharedList.innerHTML = ''
@@ -44,29 +49,38 @@ const resetPage = () => {
 
 // Control flow when "Compare" button is clicked
 const compareStrings = () => {
-  let a = inputOne.value
-  let b = inputTwo.value
-  determineUniqueFeatures(splitString(a), splitString(b))
-  loadLists()
+  let a = featuresInputOne.value
+  let b = featuresInputTwo.value
+  if (a.length > 0 && b.length > 0) {
+    processFeaturesToLists(splitString(a), splitString(b))
+    loadLists()
+  } else {
+    // createAlert() -- create a mechanism to alert errors
+  }
 }
 
 // Function to update UI after "Compare" clicked and strings parsed and compared
 const loadLists = () => {
-  uniqueToOne.forEach((word) => {
+  uniqueFeaturesArrayOne.forEach((word) => {
     let listItem = document.createElement('li')
     listItem.innerText = word
     uniqueListOne.appendChild(listItem)
   })
-  combinedFeatures.forEach((word) => {
+  combinedFeaturesArray.forEach((word) => {
     let listItem = document.createElement('li')
     listItem.innerText = word
     sharedList.appendChild(listItem)
   })
-  uniqueToTwo.forEach((word) => {
+  uniqueFeaturesArraytwo.forEach((word) => {
     let listItem = document.createElement('li')
     listItem.innerText = word
     uniqueListTwo.appendChild(listItem)
   })
+}
+
+// Function to toggle the "About" hero based on user click in header
+const toggleAbout = () => {
+  hero.classList.toggle('is-hidden')
 }
 
 // EVENT LISTENERS START
@@ -74,4 +88,6 @@ clearButton.addEventListener('click', (e) => resetPage())
 compareButton.addEventListener('click', (e) => {
   compareStrings()
 })
+aboutButton.addEventListener('click', () => toggleAbout())
+
 // EVENT LISTENERS END
